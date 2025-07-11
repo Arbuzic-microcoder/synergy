55af6a85bd314c746bd0d6ff4f923b409c0f547b
Arbuzic-microcoder <ka8359@bk.ru>
2025-07-11 13:17:35 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,8 @@
+print('Назовите стадии развитя человека от самого первого до нынешнего(После ввода названия необходимо его зафиксировать нажав Enter)') # Задание
+answer_1 = input() # 'Homo habilis'
+answer_2 = input() # 'Homo erectus'
+answer_3 = input() # 'Homo ergaster'
+answer_4 = input() # 'Homo neanderthalensis'
+answer_5 = input() # 'Homo sapiens sapiens'
+# Так лень писать всё что есть на википедии XD
+print ('Ваш ответ: '+ answer_1, answer_2, answer_3, answer_4, answer_5, sep= '=>')
